7bb5ffbdaa525b052d5923d42998721d78a0daf3
XFF <2903413325@qq.com>
2024-10-26 12:14:56 +0800

    代码优化了循环部分

--- 密码锁/密码锁.cpp
+++ 密码锁/密码锁.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 //计算前缀长度的数组
-
+int cot=0;
 vector<int>LPS(const string& s)
 {
     int len=s.length();
@@ -14,62 +14,109 @@ vector<int>LPS(const string& s)
     int i=1;
     while(i<len)
     {
-       if(s[i]==s[m]){
-           m++;
-          LPS[i]=m;
-           i++;
+        if(s[i]==s[m])
+        {
+            m++;
+            LPS[i]=m;
+            i++;
         }
-        else{
+        else
+        {
             if(m!=0)
             {
                 m=LPS[m-1];
             }
-            else{
+            else
+            {
                 LPS[i]=0;
                 i++;
             }
         }
-      }
-      return LPS;
+    }
+    return LPS;
 }
-bool open(string &parten,string & text){
- int len1=parten.length();
- int len2=text.length();
- vector<int>lps=LPS(parten);
- int i=0,j=0;
- while(i<len2){
-    if(parten[j]==text[i]){
-        i++;
-        j++;
-    }
-    if(j==len1){
-        return true;
-    }
-    else if(i<len2&&parten[j]!=text[i]){
-        if(j==0)
+bool open(string &parten,string & text)
+{
+    int len1=parten.length();
+    int len2=text.length();
+    vector<int>lps=LPS(parten);
+    int i=0,j=0;
+    while(i<len2)
+    {
+        if(parten[j]==text[i])
         {
             i++;
+            j++;
         }
-        else{
-            j=lps[j-1];
+        if(j==len1)
+        {
+            return true;
+        }
+        else if(i<len2&&parten[j]!=text[i])
+        {
+            if(j==0)
+            {
+                i++;
+            }
+            else
+            {
+                j=lps[j-1];
+            }
         }
     }
- }
-return false;
+    return false;
 }
-int main(){
- string password,text;
- cout<<"输入你原本设好的密码："<<endl;
- cin>>password;
- cout<<"输入开锁密码："<<endl;
- cin>>text;
- if(open(password,text)){
-    cout<<"开锁成功！"<<endl;
- }
-else{
-    cout<<"开锁失败!"<<endl;
+string init()
+{
+    string password;
+    cout<<"输入你原本设好的密码："<<endl;
+    cin>>password;
+    return password;
+}
+void fal(string p)
+{
 
- }
- return 0;
+    string text;
+    cin>>text;
+    if(open(p,text))
+    {
+        cout<<"开锁终于成功了！"<<endl;
+
+    }
+    else
+    {
+          cot++;
+        if(cot==4)
+        {
+            cout<<"多次输入5不成功！已无法再次输入！"<<endl;
+
+        }
+
+        else
+        {
+            cout<<"开锁还是失败了，请重输入："<<endl;
+            fal(p);
+        }
+
+
+    }
+}
+int main()
+{
+    string text;
+    string p=init();
+    cout<<"输入开锁密码："<<endl;
+    cin>>text;
+    if(open(p,text))
+    {
+        cout<<"开锁成功！"<<endl;
+    }
+    else
+    {
+        cout<<"开锁失败！重新输入："<<endl;
+        fal(p);
+
+    }
+    return 0;
 }
 
